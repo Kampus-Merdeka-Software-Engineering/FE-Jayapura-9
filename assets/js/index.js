0d8c3c5360494d4ed1abf12e152bc3e51ff48c8a
Jibalfa <najibalfarizi5@gmail.com>
2023-10-10 06:09:51 +0700

    fix contact form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,3 @@
-// Fetch data untuk header
-// function fetchHeaderData() {
-//   fetch("")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const headerElement = document.querySelector("header .header-text");
-//       headerElement.querySelector("h1").textContent = data.title;
-//       headerElement.querySelector("p").textContent = data.description;
-//     })
-//     .catch((error) => console.error("Error fetching header data:", error));
-// }
-
 function postContactFormData(event) {
   event.preventDefault();
 
@@ -52,5 +40,5 @@ function postContactFormData(event) {
 
 // Event listener untuk form submission
 document
-  .querySelector(".contact-form form")
+  .querySelector(".contact-form")
   .addEventListener("submit", postContactFormData);
